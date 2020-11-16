873c2eec0ee91db5c4eb358d170dbbbe342dbea1
Eric Mammoser <ermammoser@gmail.com>
2020-11-16 15:42:31 -0500

    Added login timeout to sqlcmd

--- app/create-sql-tables/entrypoint.sh
+++ app/create-sql-tables/entrypoint.sh
@@ -3,11 +3,7 @@
 echo 'Starting clean database...'
 /opt/mssql/bin/sqlservr &
 
-echo 'Sleeping 15 seconds to allow database to finish starting up...'
-sleep 15
-
 echo 'Setuping up the FireFighter table...'
 ./sqlfiles/setup-database.sh 
 
-echo 'Database is setup.'
 wait
--- app/create-sql-tables/setup-database.sh
+++ app/create-sql-tables/setup-database.sh
@@ -2,6 +2,13 @@
 
 echo 'Connecting database in container and create the initial db...'
 
-/opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P Password!123 -d master -i ./sqlfiles/createDB.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P Password!123 -d master -i ./sqlfiles/createDB.sql -l 300
 
-echo 'Initial db created!'
+if [[ $? = 0 ]];
+then
+	echo 'Initial db created!'
+	exit 0
+else
+	echo 'Inital db creation failed!'
+	exit 1
+fi
