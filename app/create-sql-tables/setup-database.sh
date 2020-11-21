6115d6559d432ec757ac0e83e6ec47d8677e224c
jdavis470 <jdavis470@gatech.edu>
2020-11-21 15:32:01 -0600

    Hopefully fix DB setup sequencing issue

--- app/create-sql-tables/setup-database.sh
+++ app/create-sql-tables/setup-database.sh
@@ -1,14 +1,19 @@
 # setup-database.sh
 
-echo 'Connecting database in container and create the initial db...'
+# Loop courtesy of: https://github.com/microsoft/mssql-docker/issues/11
 
+# Wait for it to be available
+echo "Waiting for MS SQL to be available ⏳"
+/opt/mssql-tools/bin/sqlcmd -l 30 -S localhost,1433  -U sa -P Password!123 -d master -Q "SET NOCOUNT ON SELECT \"YAY WE ARE UP\" , @@servername"
+is_up=$?
+while [ $is_up -ne 0 ]
+do
+  echo `date`
+  /opt/mssql-tools/bin/sqlcmd -l 30 -S localhost,1433  -U sa -P Password!123 -d master -Q "SET NOCOUNT ON SELECT \"YAY WE ARE UP\" , @@servername"
+  is_up=$?
+  sleep 5
+done
+
+echo 'Connecting database in container and create the initial db...'
 /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -U sa -P Password!123 -d master -i ./sqlfiles/createDB.sql -l 300
 
-if [[ $? = 0 ]];
-then
-	echo 'Initial db created!'
-	exit 0
-else
-	echo 'Inital db creation failed!'
-	exit 1
-fi
