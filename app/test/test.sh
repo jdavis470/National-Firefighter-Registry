8f75a877b531250a758c0fddf5f632e6e1efd39b
joeysk2012 <kurokaw1@gmail.com>
2020-11-22 16:33:47 -0600

    added functionality to test.sh

--- app/test/test.sh
+++ app/test/test.sh
@@ -63,3 +63,21 @@ do
 		echo FAILED - Check log file: $LOGFILE
 	fi
 done
+echo === Testing the case where Patient is inserted before observations ===
+FILES="$(dirname $(realpath -s $0))/data/team_created/patient_with_cancer_obs_JSON/1_patient2_patientResource.json
+$(dirname $(realpath -s $0))/data/team_created/patient_with_cancer_obs_JSON/2_Patient2_ObservationResource.json
+$(dirname $(realpath -s $0))/data/team_created/patient_with_cancer_obs_JSON/3_patient2_patientBundleResource.json
+$(dirname $(realpath -s $0))/data/team_created/patient_with_cancer_obs_JSON/4_observation2_observationBundleResource.json"
+for file in $FILES;
+do
+     	echo === Starting Test with file $file ===
+        printf "\n\n=== Starting test with $file ===" >> $LOGFILE
+
+	python3 $(dirname $(realpath -s $0))/FHIR_verifyDB.py $file &>> $LOGFILE
+	if [[ $? == 0 ]];
+	then
+		echo PASSED
+	else
+		echo FAILED - Check log file: $LOGFILE
+	fi
+done
